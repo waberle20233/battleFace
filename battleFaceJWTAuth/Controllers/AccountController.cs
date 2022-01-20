bfb48fbd3eb97018f1b7431ebcb58adf552dc0d1
WillAgvise <willa@agvise.com>
2022-01-20 13:23:25 -0600

    adding exception

--- battleFaceJWTAuth/Controllers/AccountController.cs
+++ battleFaceJWTAuth/Controllers/AccountController.cs
@@ -104,7 +104,7 @@ namespace battleFaceJWTAuth.Controllers
             }
             catch(NullReferenceException ex)
             {
-                return BadRequest();
+                return BadRequest(ex);
             }
         }
 
